c1994a4e38edc9d89eb5fc44d23d7a3683d07de5
nayeemmehedi <boyneel79@gmail.com>
2024-03-12 13:20:22 +0600

    time complexity basic

---
+++ timeComplexity.js
@@ -0,0 +1,34 @@
+big(O);
+/** 
+ * There is so many notation
+ 
+ * 
+ * O(1) - Constant time complexity.
+
+Example: Accessing an element by index in an array.
+
+
+O(log n) - Logarithmic time complexity.
+
+Example: Binary search in a sorted array.
+O(n) - Linear time complexity.
+
+Example: Finding an element in an unsorted array through linear search.
+O(n log n) - Linearithmic time complexity.
+
+Example: Most efficient comparison-based sorting algorithms like Merge Sort and Heap Sort.
+O(n^2) - Quadratic time complexity.
+
+Example: Nested loops iterating over the same collection.
+O(n^3) - Cubic time complexity.
+
+Example: Three nested loops iterating over the same collection.
+O(2^n) - Exponential time complexity.
+
+Example: Recursive algorithms that solve problems through a tree structure, like the recursive solution to the Tower of Hanoi problem.
+O(n!) - Factorial time complexity.
+
+Example: Algorithms generating all permutations of a set.   
+ * 
+ * 
+ */
